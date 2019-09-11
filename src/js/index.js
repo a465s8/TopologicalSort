bd0ccd40ebe1ffe45d73dc5f0436bae4bd0deda6
toorevitimirp <1345569965@gmail.com>
2019-09-11 22:22:02 +0800

    解决了一个一个愚蠢的致命的bug＇

--- src/js/index.js
+++ src/js/index.js
@@ -79,6 +79,7 @@ Graph.prototype.set_in_degree_o_vertexs=function(node){
     }
     return flag;
 }
+
 Graph.prototype.update_result = function rec(last_result,node){
     if(node.father!=null){
         last_result.unshift(node.vertex);
@@ -86,8 +87,12 @@ Graph.prototype.update_result = function rec(last_result,node){
     }
 }
 
-Graph.prototype.topo_sort = function rec(context,node){
-    
+Graph.prototype.topo_sort = function rec(context,node,max){
+    //checkNode(node);
+    console.log(max,context.sort_result.last_key)
+    if(context.sort_result.last_key>max){
+        return;
+    }
     var isDGA=context.set_in_degree_o_vertexs(node);
     if(isDGA && context.sort_result.last_key!=-1){
         if(context.in_degree_0_vertexs.length>0){
@@ -98,13 +103,14 @@ Graph.prototype.topo_sort = function rec(context,node){
                 node.children.push(child);
             }
             node.children.forEach(function(element){
-                rec(context,element);
+                rec(context,element,max);
             })
         }else{
             var last_key = context.sort_result.last_key;
             context.sort_result[last_key] = [];
             context.update_result(context.sort_result[last_key],node);
             context.sort_result.last_key += 1;
+            return;
         }
     }else{
         context.sort_result.last_key = -1;
@@ -112,9 +118,9 @@ Graph.prototype.topo_sort = function rec(context,node){
     }
     
 }
-Graph.prototype.draw_results = function(element,max){
+Graph.prototype.draw_results = function(element){
     if(this.sort_result.last_key!=-1){
-        for(let key = 0;key<this.sort_result.last_key && key<max;key++){
+        for(let key = 0;key<this.sort_result.last_key ;key++){
             var graph_str =  'digraph graphname{' + this.sort_result[key][0].name; 
             for(var i=1;i<this.sort_result[key].length;i++){
                 graph_str = graph_str  + ' -> '+this.sort_result[key][i].name ;
@@ -154,27 +160,59 @@ Graph.prototype.draw_results = function(element,max){
         return false;
     }
 }
-// function main(){
-//     var v1 = new Vertex('V1');
-//     var v2 = new Vertex('V2');
-//     var v3 = new Vertex('V3');
-//     var v4 = new Vertex('V4');
-//     // var v5 = new Vertex('V5');
+function test(){
+    var v1 = new Vertex('微积分B1');
+    var v2 = new Vertex('线性代数');
+    var v3 = new Vertex('微积分B2');
+    var v4 = new Vertex('离散数学');
+    var v5 = new Vertex('计算机科学导论');
+    var v6 = new Vertex('程序设计基础');
+    var v7 = new Vertex('高等微积分');
+    
   
-//     var e1 = new Edge(v1,v2);
-//     var e2 = new Edge(v1,v3);
-//     var e3 = new Edge(v1,v4);
-//     var e4 = new Edge(v4,v1);
-//     // var e4 = new Edge(v2,v5);  
-//     // var e5 = new Edge(v3,v4);
+    var v8 = new Vertex('概率论与数理统计');
+    var v9 = new Vertex('数字逻辑');
+    var v10 = new Vertex('面向对象程序设计');
+    var v11 = new Vertex('计算机组织与体系结构');
+    var v12 = new Vertex('数据结构与算法');
+    var v13 = new Vertex('计算机图形学');
+    var v14 = new Vertex('操作系统原理');
+    var v15 = new Vertex('数据库原理');
+    var v16 = new Vertex('编译原理');
+    var v17 = new Vertex('计算机网络');
+    var v18 = new Vertex('软件工程');
+    var v19 = new Vertex('智能系统');
+    // var v5 = new Vertex('V5');
+  
+    var e1 = new Edge(v1,v3);
+    // var e2 = new Edge(v1,v4);
+    // var e3 = new Edge(v2,v4);
+    // var e4 = new Edge(v3,v7);
+    // var e5 = new Edge(v3,v8);
+    // var e6 = new Edge(v4,v9);
+    // var e7 = new Edge(v5,v9);
+    // var e8 = new Edge(v6,v9);
+    // var e9 = new Edge(v5,v10);
+    // var e10 = new Edge(v6,v10);
+    // var e11 = new Edge(v9,v11);
+    // var e12 = new Edge(v10,v12);
+    // var e13 = new Edge(v10,v13);
+    // var e14 = new Edge(v12,v14);
+    // var e15 = new Edge(v12,v15);
+    // var e16 = new Edge(v12,v16);
+    // var e17 = new Edge(v12,v17);
+    // var e18 = new Edge(v12,v18);
+    // var e19 = new Edge(v12,v19);
+    // var e4 = new Edge(v2,v5);  
+    // var e5 = new Edge(v3,v4);
  
 
-//     var vertexs = [v1,v2,v3,v4]
-//     var edges = [e1,e2,e3,e4]
-//     var g = new Graph(vertexs,edges);
-//     var t = new ResultTree();
-//     g.topo_sort(g,t.root);
-//     // t.get_sort_result_list(g.sort_result,t.root);
-//     console.log(g.sort_result);
-// }
-//  main();
+    var vertexs = [v1,v2,v3,v4,v5,v6,v7,v8,v9,v10,v11,v12,v13,v14,v15,v16,v17,v18,v19];
+    var edges = [e1];
+    var g = new Graph(vertexs,edges);
+    var t = new ResultTree();
+    g.topo_sort(g,t.root);
+    // t.get_sort_result_list(g.sort_result,t.root);
+    // console.log(g.sort_result);
+}
+ //test();
